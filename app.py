62dd92210761055751fc356b20fca22d263af8ea
stefaneb <stefanbe236@nemi.tskoli.is>
2018-03-15 14:44:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from bottle import run, route, template, static_file, error, request
 
 @route('/')
@@ -28,4 +29,4 @@ def order():
     ananas = request.query.ananas
     return template('order', fullname=fullname, pizzasize=pizzasize, skinka=skinka, pepperoni=pepperoni, ananas=ananas)
 
-run()
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
